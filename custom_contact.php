e20ef33ba3e961e1f020fdfad68d140211df6cb0
rmadziyauswa <rmadziyauswa@users.noreply.github.com>
2014-05-17 13:47:37 +0200

    Update custom_contact.php

--- custom_contact.php
+++ custom_contact.php
@@ -13,15 +13,16 @@ License: GPL
 This program is free software; you can redistribute it 
 */
 
-
 function custom_contact_print()
 {
 
-	if(!is_admin())
+		
+		
+		if(!is_admin())
 	{
 		echo "<h2>Contact Us</h2>";
-		echo "<div><form action='sendcontact.php' method='post'><legend>Fill in the fields and send</legend>
-		<label for='cName'>Full Name</label><input type='text' name='cName' id='cName' />
+		echo "<div><form action='' method='post'><legend>Fill in the fields and send</legend>
+		<label for='cSubject'>Subject</label><input type='text' name='cSubject' id='cSubject' />
 		<label for='cEmail'>Email</label><input type='text' name='cEmail' id='cEmail' />
 		<label for='cMessage'>Message</label><textarea name='cMessage' id='cMessage'></textarea>
 		<input type='submit' name='btnSubmit' id='btnSubmit' value='Send'/>
@@ -33,8 +34,11 @@ function custom_contact_print()
 
 function custom_contact_menu_page()
 {
-	echo "<h2>Contact Form Entries</h2>";
-	echo "<table><tr><th>Name</th><th>Email</th><th>Message</th></tr></table>";
+	echo "<h2>Contact Form Details</h2>";
+	echo "<p>Just put this short code in the page you would like to show the contact form [custom_contact]</p>
+	<p>Please Note that the info entered on the contact form will be sent 
+	to the blog administrators email address <strong>(". get_option('admin_email').")</strong></p>";
+
 }
 
 
@@ -43,37 +47,25 @@ function custom_contact_menu()
 	add_menu_page("Contact Form Admin","Custom Contact Form","manage_options","ccf_menu","custom_contact_menu_page");
 }
 
-register_activation_hook(__FILE__,'custom_contact_db_create');
-register_deactivation_hook(__FILE__,'custom_contact_db_drop');
-add_action("init","custom_contact_print");
 add_action("admin_menu","custom_contact_menu");
+add_shortcode('custom_contact','custom_contact_print');
 
-function custom_contact_db_create()
+
+
+
+//send an email to the blog admin if the send button on contact form is pressed
+if(!is_admin() && isset($_POST['cSubject']))
 {
-	global $wpdb;
-	
-	$tablename = $wpdb->prefix."custom_contact";
-	
-	if($wpdb->get_var('SHOW TABLES LIKE '.$tablename) != $tablename)
+	$to = get_option('admin_email');
+	$subject = $_POST['cSubject'];
+	$message = $_POST['cMessage'];
+
+	if(mail($to,$subject,$message))
 	{
-		$sql = 'CREATE TABLE '. $tablename .'(
-		id INT UNSIGNED AUTO_INCREMENT,
-		ptime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		fullname VARCHAR(100),
-		email VARCHAR(50),
-		message VARCHAR(255),
-		PRIMARY KEY (id)
-		)';
-		
-		require_once(ABSPATH.'wp-admin/includes/upgrade.php');
-		dbDelta($sql);
-		
+		echo "Sent";
 	}
-}
-
-
-function custom_contact_db_drop()
-{
-	global $wpdb;
-	$wpdb->get_var('DROP TABLE '.$tablename) ;
+	else
+	{
+		echo "Mail Not Sent";
+	}
 }
